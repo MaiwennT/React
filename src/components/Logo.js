58ea67f0a7519448f4cba0875c424ac55b908db0
MaiwennT <maiwenn.tanguy@outlook.com>
2022-09-06 14:42:23 +0200

    done.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Logo = () => {
   return (
     <div className="logo">
       {/* Les images importées depuis la balise IMG sont accessibles dans "public" */}
-      <img src="./logo192.png" alt="logo react" />
-      <h3>React World</h3>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 };
